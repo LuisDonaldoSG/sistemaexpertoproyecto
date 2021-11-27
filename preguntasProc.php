b0f4ae4062f2aa0f4be931eb7cccb6a8e25982e1
Donaldo <51167382+LuisDonaldoSG@users.noreply.github.com>
2021-11-27 13:00:30 -0600

    pintado de las materias listo

--- preguntasProc.php
+++ preguntasProc.php
@@ -1,6 +1,8 @@
 <?php
 require('preguntas.php');
 
+$contadorMaterias = 0;
+
 foreach($cuestionario as $index => $cosas ) {
 
     //echo 'respuesta '.$_POST[$index]. "\n";    
@@ -46,8 +48,8 @@ foreach($prioridades as $w => $x) {
             $nuevo_plan[$y] = $z;            
         }
     }
+
 }
-var_dump($nuevo_plan)
 ?>
 
 <!DOCTYPE html>
@@ -56,9 +58,232 @@ var_dump($nuevo_plan)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Resultado</title>
 </head>
 <body>
-    
+<div class="container mt-5 mb-5">
+    <h2 class = "text-center mb-5">Materias asignadas</h2>
+
+    <?php
+        $contadorMaterias = 0;
+        foreach ($nuevo_plan as $key => $value) {
+            if ($value == 'troncocomun'){
+                $contadorMaterias = $contadorMaterias + 1;
+            }
+        }
+    ?>
+
+    <?php
+        if ($contadorMaterias > 0){
+    ?> 
+
+        <h5 class = "text-center mb-3">Materias de tronco común</h5>
+            <div class="alert alert-warning d-flex align-items-center" role="alert">    
+                <div>
+                    <ul >
+                            <?php
+                                foreach ($nuevo_plan as $key => $value) {
+                                    if ($value == 'troncocomun'){
+                            ?>
+                                <li>
+                                <?php echo $key ?> 
+                                </li>
+                            <?php
+                                    }        
+                                }
+                            ?>
+                    </ul>
+                </div>
+            </div>
+
+    <?php
+        }
+    ?>
+
+    <?php
+        $contadorMaterias = 0;
+        foreach ($nuevo_plan as $key => $value) {
+            if ($value == 'formacioprofecional'){
+                $contadorMaterias = $contadorMaterias + 1;
+            }
+        }
+    ?>
+
+    <?php
+        if ($contadorMaterias > 0){
+    ?> 
+
+        <h5 class = "text-center mb-3">Materias de formación profecional</h5>
+            <div class="alert alert-warning d-flex align-items-center" role="alert">    
+                <div>
+                    <ul >
+                            <?php
+                                foreach ($nuevo_plan as $key => $value) {
+                                    if ($value == 'formacioprofecional'){
+                            ?>
+                                <li>
+                                <?php echo $key ?> 
+                                </li>
+                            <?php
+                                    }        
+                                }
+                            ?>
+                    </ul>
+                </div>
+            </div>
+
+    <?php
+        }
+    ?>
+
+
+<?php
+        $contadorMaterias = 0;
+        foreach ($nuevo_plan as $key => $value) {
+            if ($value == 'especialidadanalistadedatos'){
+                $contadorMaterias = $contadorMaterias + 1;
+            }
+        }
+    ?>
+
+    <?php
+        if ($contadorMaterias > 0){
+    ?> 
+
+        <h5 class = "text-center mb-3">Materias de la especialidad en analista de datos</h5>
+            <div class="alert alert-warning d-flex align-items-center" role="alert">    
+                <div>
+                    <ul >
+                            <?php
+                                foreach ($nuevo_plan as $key => $value) {
+                                    if ($value == 'especialidadanalistadedatos'){
+                            ?>
+                                <li>
+                                <?php echo $key ?> 
+                                </li>
+                            <?php
+                                    }        
+                                }
+                            ?>
+                    </ul>
+                </div>
+            </div>
+
+    <?php
+        }
+    ?>
+
+<?php
+        $contadorMaterias = 0;
+        foreach ($nuevo_plan as $key => $value) {
+            if ($value == 'especialidadadministraciondeproyectosinformaticos'){
+                $contadorMaterias = $contadorMaterias + 1;
+            }
+        }
+    ?>
+
+    <?php
+        if ($contadorMaterias > 0){
+    ?> 
+
+        <h5 class = "text-center mb-3">Materias de la especialidad de administración de proyectos informaticos</h5>
+            <div class="alert alert-warning d-flex align-items-center" role="alert">    
+                <div>
+                    <ul >
+                            <?php
+                                foreach ($nuevo_plan as $key => $value) {
+                                    if ($value == 'especialidadadministraciondeproyectosinformaticos'){
+                            ?>
+                                <li>
+                                <?php echo $key ?> 
+                                </li>
+                            <?php
+                                    }        
+                                }
+                            ?>
+                    </ul>
+                </div>
+            </div>
+
+    <?php
+        }
+    ?>
+
+<?php
+        $contadorMaterias = 0;
+        foreach ($nuevo_plan as $key => $value) {
+            if ($value == 'especialidadciberseguridadyadministracionderedes'){
+                $contadorMaterias = $contadorMaterias + 1;
+            }
+        }
+    ?>
+
+    <?php
+        if ($contadorMaterias > 0){
+    ?> 
+
+        <h5 class = "text-center mb-3">Materias de la especialidad en ciberseguridad y administración de redes </h5>
+            <div class="alert alert-warning d-flex align-items-center" role="alert">    
+                <div>
+                    <ul >
+                            <?php
+                                foreach ($nuevo_plan as $key => $value) {
+                                    if ($value == 'especialidadciberseguridadyadministracionderedes'){
+                            ?>
+                                <li>
+                                <?php echo $key ?> 
+                                </li>
+                            <?php
+                                    }        
+                                }
+                            ?>
+                    </ul>
+                </div>
+            </div>
+
+    <?php
+        }
+    ?>
+
+<?php
+        $contadorMaterias = 0;
+        foreach ($nuevo_plan as $key => $value) {
+            if ($value == 'optativas'){
+                $contadorMaterias = $contadorMaterias + 1;
+            }
+        }
+    ?>
+
+    <?php
+        if ($contadorMaterias > 0){
+    ?> 
+
+        <h5 class = "text-center mb-3">Materias optativas</h5>
+            <div class="alert alert-warning d-flex align-items-center" role="alert">    
+                <div>
+                    <ul >
+                            <?php
+                                foreach ($nuevo_plan as $key => $value) {
+                                    if ($value == 'optativas'){
+                            ?>
+                                <li>
+                                <?php echo $key ?> 
+                                </li>
+                            <?php
+                                    }        
+                                }
+                            ?>
+                    </ul>
+                </div>
+            </div>
+
+    <?php
+        }
+    ?>
+
+</div>
+
 </body>
 </html>
